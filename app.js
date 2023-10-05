3db391d077e4c0c15574dc4574c93331ea89b8bd
Aaditi Akolkar <78417472+aditiaakolkar@users.noreply.github.com>
2023-10-05 16:47:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,122 @@
+//Assignment 1: Array Operations
+//-------------------------------------------------------------------------------------
+
+//1. Create an empty array called "Fruits".
+// let Fruits = [];
+// console.log(Fruits);
+
+// 2. Add the following Fruits to the "Fruits" array: "apple", "banana", "orange".
+// let Fruits = ['Apple', 'Banana', 'Orange'];
+// console.log(Fruits);
+
+//3. Remove the first Fruit from the array.
+// let Fruits = ['Apple', 'Banana', 'Orange'];
+// Fruits.splice(0, 1);
+// console.log(Fruits);
+
+//4. Add "Grape" to the end of the array.
+// let Fruits = ['Apple', 'Banana', 'Orange'];
+// Fruits.push('Grapes');
+// console.log(Fruits);
+
+//5. Update the second Fruit in the array to "Pear".
+// let Fruits = ['Apple', 'Banana', 'Orange'];
+// Fruits.splice(1, 0, 'Pear')
+// console.log(Fruits);
+
+//6. Print the final "Fruits" array after performing the above operations.
+// let Fruits = ['Apple', 'Banana', 'Orange'];
+// Fruits.splice(0, 1);
+// Fruits.push('Grapes');
+// Fruits.splice(1, 0, 'Pear');
+
+// console.log(Fruits);
+
+//-------------------------------------------------------------------------------------
+//Assignment 2: Object Operations
+//-------------------------------------------------------------------------------------
+
+//1. Create an empty object called "person".
+// let person = {};
+// console.log(person);
+
+//2. Add the following properties to the "person" object:
+// - name: "John"
+// - age: 30
+// - city: "New York"
+
+// let person = {Name:'John', Age: 30, City: 'New York'};
+// console.log(person);
+
+// 3. Remove the "age" property from the "person" object.
+// let person = {Name:'John', Age: 30, City: 'New York'};
+// delete person.Age;
+// console.log(person);
+
+// 4. Add a new property called "job" with the value "Engineer" to the "person" object.
+// let person = {Name:'John', Age: 30, City: 'New York'};
+// person.Job = 'Engineer';
+// console.log(person);
+
+// 5. Update the "city" property of the "person" object to "San Francisco".
+// let person = {Name:'John', Age: 30, City: 'New York'};
+// person.City = 'San Farncisco';
+// console.log(person);
+
+// 6. Print the final "person" object after performing the above operations.
+// let person = {Name:'John', Age: 30, City: 'New York'};
+// delete person.Age;
+// person.Job = 'Engineer';
+// person.City = 'San Farncisco';
+
+// console.log(person);
+
+//-------------------------------------------------------------------------------------
+//Assignment 3: Array of Objects Operations
+//-------------------------------------------------------------------------------------
+
+//1. Create an empty array called "Cars".
+// let Cars = [];
+// console.log(Cars);
+
+//2. Add three car objects to the "Cars" array. Each car object should have the following properties:
+// - make: "Toyota"
+// - model: "Camry"
+// - year: 2018
+
+// let Cars = {Make: 'Toyota', Model: 'Camry', Year: 2018};
+// console.log(Cars);
+
+//3. Remove the first car object from the "Cars" array.
+// let Cars = {Make: 'Toyota', Model: 'Camry', Year: 2018};
+// delete Cars.Make;
+// console.log(Cars);
+
+//4. Add a new car object to the "Cars" array with the following properties:
+// - make: "Honda"
+// - model: "Civic"
+// - year: 2020
+
+// let Cars = {Make: 'Toyota', Model: 'Camry', Year: 2018};
+// Cars.Make = 'Honda';
+// Cars.Model = 'Civic';
+// Cars.Year = 2020;
+
+// console.log(Cars);
+
+
+//5. Update the "model" property of the second car object in the array to "Accord".
+// let Cars = {Make: 'Toyota', Model: 'Camry', Year: 2018};
+// Cars.Model = 'Accord';
+// console.log(Cars);
+
+//6. Print the final "Cars" array after performing the above operations.
+// let Cars = {Make: 'Toyota', Model: 'Camry', Year: 2018};
+// delete Cars.Make;
+
+// Cars.Make = 'Honda';
+// Cars.Model = 'Civic';
+// Cars.Year = 2020;
+
+// Cars.Model = 'Accord';
+// console.log(Cars);
